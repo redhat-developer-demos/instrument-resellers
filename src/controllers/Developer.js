bd7eeeff3975f2db4b4824bee751cb5a51d2b8f5
Bob Reselman <bob@CogArtTech.com>
2022-06-03 15:23:18 -0700

    auto-genned code

---
+++ src/controllers/Developer.js
@@ -0,0 +1,144 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Developer = require('../service/DeveloperService');
+
+module.exports.getAcquisition = function getAcquisition (req, res, next, id) {
+  Developer.getAcquisition(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getAcquisitions = function getAcquisitions (req, res, next) {
+  Developer.getAcquisitions()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getInstrument = function getInstrument (req, res, next, id) {
+  Developer.getInstrument(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getInstruments = function getInstruments (req, res, next) {
+  Developer.getInstruments()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getManufacturer = function getManufacturer (req, res, next, id) {
+  Developer.getManufacturer(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getManufacturers = function getManufacturers (req, res, next) {
+  Developer.getManufacturers()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getPurchase = function getPurchase (req, res, next, id) {
+  Developer.getPurchase(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getPurchases = function getPurchases (req, res, next) {
+  Developer.getPurchases()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getRefurbishment = function getRefurbishment (req, res, next, id) {
+  Developer.getRefurbishment(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getRefurbishments = function getRefurbishments (req, res, next) {
+  Developer.getRefurbishments()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getSeller = function getSeller (req, res, next, id) {
+  Developer.getSeller(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getSellers = function getSellers (req, res, next) {
+  Developer.getSellers()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getUser = function getUser (req, res, next, id) {
+  Developer.getUser(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getUsers = function getUsers (req, res, next) {
+  Developer.getUsers()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ src/service/DeveloperService.js
@@ -0,0 +1,395 @@
+'use strict';
+
+
+/**
+ * Find an Acquisition by ID
+ * Returns a single Acquisition
+ *
+ * id uuid ID of the purchase to return
+ * returns Acquisition
+ **/
+exports.getAcquisition = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Returns a list of Acquisitions
+ * Returns a list of Acquisitions
+ *
+ * returns ArrayOfAcquisitions
+ **/
+exports.getAcquisitions = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ "", "" ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find an Instrument by ID
+ * Returns a single Instrument
+ *
+ * id uuid ID of the purchase to return
+ * returns Instrument
+ **/
+exports.getInstrument = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ *
+ * returns ArrayOfInstruments
+ **/
+exports.getInstruments = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ "", "" ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find an Manufacturers by ID
+ * Returns a single Manufacturer
+ *
+ * id uuid ID of the purchase to return
+ * returns Instrument
+ **/
+exports.getManufacturer = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Returns a list of Manufacturers
+ * Returns a list of Manufacturers
+ *
+ * returns ArrayOfManufacturers
+ **/
+exports.getManufacturers = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "address" : {
+    "city" : "Anytown",
+    "zip_region_code" : "90001-1234",
+    "address_1" : "123 Maple Street",
+    "address_2" : "Unit 1",
+    "state_province" : "CA"
+  },
+  "name" : "Yamaha Instruments",
+  "id" : "id"
+}, {
+  "address" : {
+    "city" : "Anytown",
+    "zip_region_code" : "90001-1234",
+    "address_1" : "123 Maple Street",
+    "address_2" : "Unit 1",
+    "state_province" : "CA"
+  },
+  "name" : "Yamaha Instruments",
+  "id" : "id"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find Purchase by ID
+ * Returns a single Purchase
+ *
+ * id uuid ID of the purchase to return
+ * returns Purchase
+ **/
+exports.getPurchase = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "purchaseDate" : "2021-06-20T01:02:03Z",
+  "price" : 539.5,
+  "instrument" : "",
+  "id" : "id",
+  "user" : {
+    "firstName" : "Sally",
+    "lastName" : "Smith",
+    "address" : {
+      "city" : "Anytown",
+      "zip_region_code" : "90001-1234",
+      "address_1" : "123 Maple Street",
+      "address_2" : "Unit 1",
+      "state_province" : "CA"
+    },
+    "phone" : "111-222-3333",
+    "id" : "id",
+    "userType" : "",
+    "email" : "sally.smith@example.com"
+  }
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Gets a list of Purchases
+ * Gets a list of Purchases
+ *
+ * returns ArrayOfPurchases
+ **/
+exports.getPurchases = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "purchaseDate" : "2021-06-20T01:02:03Z",
+  "price" : 539.5,
+  "instrument" : "",
+  "id" : "id",
+  "user" : {
+    "firstName" : "Sally",
+    "lastName" : "Smith",
+    "address" : {
+      "city" : "Anytown",
+      "zip_region_code" : "90001-1234",
+      "address_1" : "123 Maple Street",
+      "address_2" : "Unit 1",
+      "state_province" : "CA"
+    },
+    "phone" : "111-222-3333",
+    "id" : "id",
+    "userType" : "",
+    "email" : "sally.smith@example.com"
+  }
+}, {
+  "purchaseDate" : "2021-06-20T01:02:03Z",
+  "price" : 539.5,
+  "instrument" : "",
+  "id" : "id",
+  "user" : {
+    "firstName" : "Sally",
+    "lastName" : "Smith",
+    "address" : {
+      "city" : "Anytown",
+      "zip_region_code" : "90001-1234",
+      "address_1" : "123 Maple Street",
+      "address_2" : "Unit 1",
+      "state_province" : "CA"
+    },
+    "phone" : "111-222-3333",
+    "id" : "id",
+    "userType" : "",
+    "email" : "sally.smith@example.com"
+  }
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find Purchase by ID
+ * Returns a single Refurbishments
+ *
+ * id uuid ID of the Refurbishments to return
+ * returns Refurbishment
+ **/
+exports.getRefurbishment = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Gets a list of Refurbishments
+ * Gets a list of Refurbishments
+ *
+ * returns ArrayOfRefurbishments
+ **/
+exports.getRefurbishments = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ "", "" ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find a Seller by ID
+ * Returns a single Seller
+ *
+ * id uuid ID of the seller to return
+ * returns Seller
+ **/
+exports.getSeller = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = "";
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Returns a list of Sellers
+ * Returns a list of Sellers
+ *
+ * returns ArrayOfSellers
+ **/
+exports.getSellers = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ "", "" ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Find User by ID
+ * Returns a single User
+ *
+ * id uuid ID of the purchase to return
+ * returns User
+ **/
+exports.getUser = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "firstName" : "Sally",
+  "lastName" : "Smith",
+  "address" : {
+    "city" : "Anytown",
+    "zip_region_code" : "90001-1234",
+    "address_1" : "123 Maple Street",
+    "address_2" : "Unit 1",
+    "state_province" : "CA"
+  },
+  "phone" : "111-222-3333",
+  "id" : "id",
+  "userType" : "",
+  "email" : "sally.smith@example.com"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Gets a list of Users
+ * Gets a list of Users
+ *
+ * returns ArrayOfUsers
+ **/
+exports.getUsers = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "firstName" : "Sally",
+  "lastName" : "Smith",
+  "address" : {
+    "city" : "Anytown",
+    "zip_region_code" : "90001-1234",
+    "address_1" : "123 Maple Street",
+    "address_2" : "Unit 1",
+    "state_province" : "CA"
+  },
+  "phone" : "111-222-3333",
+  "id" : "id",
+  "userType" : "",
+  "email" : "sally.smith@example.com"
+}, {
+  "firstName" : "Sally",
+  "lastName" : "Smith",
+  "address" : {
+    "city" : "Anytown",
+    "zip_region_code" : "90001-1234",
+    "address_1" : "123 Maple Street",
+    "address_2" : "Unit 1",
+    "state_province" : "CA"
+  },
+  "phone" : "111-222-3333",
+  "id" : "id",
+  "userType" : "",
+  "email" : "sally.smith@example.com"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
