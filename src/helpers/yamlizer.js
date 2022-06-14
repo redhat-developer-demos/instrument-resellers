0a195db46d7edc36f776d1d074e4b86449c2b4c6
Bob Reselman <bob@CogArtTech.com>
2022-06-14 09:37:37 -0700

    fixing config bug

--- src/helpers/yamlizer.js
+++ src/helpers/yamlizer.js
@@ -15,14 +15,20 @@ const modifyOpenApiSpecToVendorSync = (config) => {
     if (! config.inputYamlPath) throw new Error("No config.inputYamlPath defined");
     if (! config.outputYamlPath) throw new Error("No config.outputYamlPath defined");
     //if (! config.vendorName) throw new Error("No config.vendorName defined");
-    if (! config.serverHost) throw new Error("No config.serverHost defined");
-    if (! config.serverPort) throw new Error("No config.serverPort defined");
+    //if (! config.serverHost) throw new Error("No config.serverHost defined");
+
+    const serverHost = config.serverHost || "http://localhost";
+    const serverPort = config.serverHost || 8088;
+    const vendorName = config.vendorName || "Unknown";
+    //if (! config.serverPort) throw new Error("No config.serverPort defined");
 
     const obj = yaml.load(fs.readFileSync(config.inputYamlPath, {encoding: 'utf-8'}));
-    //TODO change the value as needed
+    //TODO clean up environment variable dependency processing
+    obj.info.title = vendorName
+    //if (config.vendorName) obj.info.title = config.vendorName;
+    //if ( config.serverHost &&  config.serverPort ) obj.servers[0]= {url: `${config.serverHost}:${config.serverPort}/v1`}
 
-    if (config.vendorName) obj.info.title = config.vendorName;
-    if ( config.serverHost &&  config.serverPort ) obj.servers[0]= {url: `${config.serverHost}:${config.serverPort}/v1`}
+    obj.servers[0]= {url: `${serverHost}:${serverPort}/v1`}
 
     let yamlStr = yaml.safeDump(obj);
     fs.writeFileSync(config.outputYamlPath, yamlStr, 'utf8');
